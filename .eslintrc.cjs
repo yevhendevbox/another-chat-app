f7e7031f31d432103cc2f8157d34b8947960ddfb
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-05-26 11:01:23 +0300

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  rules: {
+    'linebreak-style': 'off',
+    'max-len': ['error', { code: 120, tabWidth: 2 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-useless-escape': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    HelloWorld from APP
+  </div>
+</template>
+
+<style scoped>
+</style>
