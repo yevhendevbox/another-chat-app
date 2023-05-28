a9dd440b55df959cd46e07e6112f2e6c6de0ec48
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2023-05-28 19:04:31 +0300

    ref: minor librarise setting up

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    HelloWorld from APP
-  </div>
+  <h1 class="text-red-600">HelloWorld from APP</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -16,7 +16,7 @@ const router = createRouter({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import('../views/AboutView.vue')
     // }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
