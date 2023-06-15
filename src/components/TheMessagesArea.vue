b09680dbcc3b7a0566ff5de693c76350ac83dee9
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2023-06-15 17:45:32 +0300

    ref: remove comments from code

--- src/components/TheMessagesArea.vue
+++ src/components/TheMessagesArea.vue
@@ -3,37 +3,6 @@
     <div class="flex flex-col h-full">
       <div class="p-4 flex-grow">
         <h2 class="text-lg font-semibold mb-4">Повідомлення</h2>
-        <!-- <ul>
-          <li class="py-2">
-            <div class="flex">
-              <div class="bg-gray-200 text-gray-800 max-w-[50%] rounded-full p-2">
-                <p class="text-sm leading-normal break-words">Привіт, як справи?</p>
-              </div>
-            </div>
-          </li>
-          <li class="py-2">
-            <div class="flex justify-end">
-              <div class="bg-blue-500 text-white max-w-[50%] rounded-full p-2">
-                <p class="text-sm leading-normal break-words">Все гаразд, дякую!</p>
-              </div>
-            </div>
-          </li>
-          <li class="py-2">
-            <div class="flex justify-end">
-              <div class="bg-blue-500 text-white max-w-[50%] rounded-full p-2">
-                <p class="text-sm leading-normal break-words">А твої як?</p>
-              </div>
-            </div>
-          </li>
-          <li class="py-2">
-            <div class="flex">
-              <div class="bg-gray-200 text-gray-800 max-w-[50%] rounded-full p-2">
-                <p class="text-sm leading-normal break-words">Краще всіх!</p>
-              </div>
-            </div>
-          </li>
-        </ul> -->
-
         <ul v-if="messages.length > 0">
           <li v-for="message in messages" :key="message.id" class="py-2">
             <div :class="[message.isMyMessage ? 'justify-end' : 'justify-start', 'flex']">
