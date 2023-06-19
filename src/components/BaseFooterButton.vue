b32d7f50cb9504f14f4591d170b97438430bdc51
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2023-06-19 22:13:41 +0300

    added reusable component BaseFooterButton.vue

---
+++ src/components/BaseFooterButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <component :is="props.icon" class="h-6 w-6" />
+    <span class="ml-2">{{ props.label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const props = defineProps({
+  icon: {
+    type: String,
+    requered: true,
+  },
+  label: {
+    type: String,
+    requered: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,21 +1,13 @@
 <template>
   <footer class="sticky bottom-0 bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
-    <div class="flex">
-      <HomeIcon class="h-6 w-6" />
-      <span class="ml-2">Домівка</span>
-    </div>
-    <div class="flex">
-      <ChatBubbleBottomCenterTextIcon class="h-6 w-6" />
-      <span class="ml-2">Чати</span>
-    </div>
-    <div class="flex">
-      <CogIcon class="h-6 w-6" />
-      <span class="ml-2">Налаштування</span>
-    </div>
+    <BaseFooterButton :icon="HomeIcon" label="Домівка" />
+    <BaseFooterButton :icon="ChatBubbleBottomCenterTextIcon" label="Чати" />
+    <BaseFooterButton :icon="CogIcon" label="Налаштування" />
   </footer>
 </template>
 
 <script setup>
+import BaseFooterButton from '@/components/BaseFooterButton.vue';
 import { ChatBubbleBottomCenterTextIcon } from '@heroicons/vue/24/solid';
 import { HomeIcon } from '@heroicons/vue/24/solid';
 import { CogIcon } from '@heroicons/vue/24/solid';
